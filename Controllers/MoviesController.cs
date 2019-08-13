bee060ab35f3b06ec88d2a142bf35ad27835fafb
Ola Sjoberg <ola@sjoberg.nu>
2019-08-13 06:33:03 +0200

    A little bit further

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
+
 
 namespace Vidly.Controllers
 {
@@ -12,10 +14,28 @@ namespace Vidly.Controllers
         // GET: Movie
         public ActionResult Random()
         {
-            var movie = new Movie(){ Name = "Shrek2!" };
-            
-            return View(movie);
+            var movie = new Movie() { Name = "Shrek2!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1"},
+                new Customer { Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
             // return Json(new { name = "Shrek3!", Id=9999 }, JsonRequestBehavior.AllowGet);
+    }
+
+        [Route("movies/output/{year:regex(\\d{4})}")]
+        public ActionResult Output(int year)
+        {
+            return Content("This would be the output if it worked.. Maybe in year "+ year.ToString());
         }
     }
 }
